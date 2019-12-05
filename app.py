d208b6da481e06cb476cd23664944450f0b7f378
vlad <caraseli02@gmail.com>
2019-12-05 17:47:10 +0100

    Crear conexion con base de datos, Modicar Css

--- app.py
+++ app.py
@@ -2,8 +2,17 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+#Paquete para Base de datos
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+#Mongo URL Atlas
+MONGO_URL_ATLAS = 'mongodb+srv://admin:root@cluster0-ypkpe.azure.mongodb.net/test?retryWrites=true&w=majority'
+
+client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
+db = client['Inserted']
+collection = db['Worlds']
 
 #rutas
 @app.route('/', methods=['GET', 'POST'])
