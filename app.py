e167303b3794869dfaa28d33877c59e13abe7017
vlad <caraseli02@gmail.com>
2019-12-07 09:36:21 +0100

    gurdar cambios

--- app.py
+++ app.py
@@ -27,9 +27,11 @@ def index():
     if request.method == 'POST':
         title = request.form.get('title')
         nota = request.form.get('nota')
-        # collection.delete_many({})
-        collection.insert_one({'title': title, 'nota': nota, 'date': d})
-        return render_template('index.html')
+        if title == '' and nota == '':
+            return render_template('index.html')
+        else:
+            collection.insert_one({'title': title, 'nota': nota, 'date': d})
+            return render_template('index.html')
     return render_template('index.html',)
 
 @app.route('/verNotas', methods=['GET', 'POST'])
@@ -48,5 +50,27 @@ def ver():
 
     return render_template('verNotas.html')
 
+@app.route('/borrarNotas', methods=['GET', 'POST'])
+def borrarNotas():
+    global dateInfo
+    d = datetime.date(random.randrange(1900, 2020), random.randrange(1, 13), random.randrange(1, 31))
+    d = str(d)
+    if request.method == 'POST':
+        title = request.form.get('title')
+        nota = request.form.get('nota')
+        collection.delete_one( {'title': title, 'nota': nota})
+        return render_template('borrarNotas.html')
+    return render_template('borrarNotas.html',)
+
+@app.route('/actualiarNotas', methods=['GET', 'POST'])
+def actualiarNotas():
+    global dateInfo
+    d = datetime.date(random.randrange(1900, 2020), random.randrange(1, 13), random.randrange(1, 31))
+    d = str(d)
+    title = request.form.get('title')
+    if request.method == 'POST':
+        if title == '':
+            return render_template('index.html')
+    return render_template('actualiarNotas.html',)
 if __name__ == "__main__":
     app.run('127.0.0.1', 5000, debug=True)
